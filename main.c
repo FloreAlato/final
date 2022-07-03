e0fbf4b80999576f02b7c2f06eed48db8fb84100
FloreAlato <davyflore1809@gmail.com>
2022-07-03 10:29:40 -0500

    di bene in meglio

--- main.c
+++ main.c
@@ -8,26 +8,24 @@ int main() {
 
     //TESTING
     int i, j, k;
-    char f;
 
     int numero_giocatori, numero_giocatori_veri;
     Elenco *giocatori = NULL;
-    bool *giocatori_vivi = NULL;
     ProfiloGiocatore *giocatori_veri;
     FILE *file = NULL;
 
-    bool nuovo, esistente;
+    bool nuovo, esistente, scelto;
     int scelta, prosegui;
     char nome[30];
     char opzioni[2][10] = {"carica", "inserisci"};
     char scelte[4][10] = {"continua", "salva", "inizia", "esci"};
-    char filename[31] = "savegame_twoPlayers_noGame.bin\0";
+    char filename[32];
     char *file_path = NULL;
     char **files = NULL;
+    int numero_files;
     int numero_profili;
 
     int id;
-    bool *gia_scelti = NULL;
 
 
 
@@ -97,28 +95,44 @@ int main() {
 
         // per caricare da un file esistente
     } else {
-        // parte dal presupposto che il file sia già stato scelto, da cambiare in futuro
-        // inoltre non legge se la partita è in corso
-        /*file_path = make_path(filename, "");
 
+        // cominciamo
+        printf("\nQuesti sono i file disponibili:");
 
-        file = fopen(file_path, "rb");
+        file = fopen("../cmake-build-debug/Save_Files/savegame_files.bin", "rb");
         if(file == NULL) {
             printf("\n\nERRORE! Impossibile aprire file!");
             exit(-1);
         }
 
-        fread(&numero_profili, sizeof(int), 1, file);
+        fread(&numero_files, sizeof(int), 1, file);
 
-        giocatori_veri = (ProfiloGiocatore *) calloc(numero_profili, sizeof(ProfiloGiocatore));
-        leggi_giocatori(file, numero_profili, giocatori_veri);
-
-        for(i = 0; i < numero_profili; i++) {
-            printf("\n%s", giocatori_veri[i].nome);
+        files = (char **) calloc(sizeof(char *), numero_files);
+        if(files == NULL) {
+            printf("\n\nERRORE! Allocazione fallita!");
+            exit(-1);
         }
 
+        for(k = 0; k < numero_files; k++) {
+            files[k] = (char *) calloc(sizeof(char), 32);
+            if(files[k] == NULL) {
+                printf("\n\nERRORE! Allocazione fallita!");
+                exit(-1);
+            }
 
-        fclose(file);*/
+            fread(files[k], sizeof(char), 32, file);
+        }
+
+        fclose(file);
+
+        for(k = 0; k < numero_files; k++) {
+            printf("\n[%d] -> %s", k, files[k]);
+        }
+
+        // scegliere il file e recuperare le info
+
+
+        free(files);
     }
 
 
@@ -141,19 +155,49 @@ int main() {
     }
 
 
-    /*printf("\n\nQuanti di questi %d profini vuoi usare: ", numero_profili);
+    printf("\n\nQuanti di questi %d profini vuoi usare: ", numero_profili);
     numero_giocatori_veri = get_int("", 0, numero_profili);
 
 
     // scelta del profilo al quale assegnare il giusto indice
-    for(i = 0; i < numero_giocatori_veri; i++) {
-
-        // il giocatore seleziona i profili da usare e gli assegna un id
-        // code
-    }*/
+    for(j = 0; j < numero_giocatori_veri; j++) {
 
 
+        // debuggare questa parte, e' ancora possibile scegliere lo stesso profilo due volte
+        // inoltre li stampa tutti ogni volta
 
+        printf("\nProfili a disposizione:");
+
+        for(i = 0; i < numero_profili; i++) {
+
+            printf("\n[%d] -> %s", i, giocatori_veri[i].nome);
+        }
+
+        scelta = get_int("\n\n[Tu]: ", 0, numero_giocatori_veri);
+
+        // debuggato, non puoi inserire lo stesso id per due profili
+        do {
+            scelto = false;
+            printf("\nId di %s: ", giocatori_veri[scelta].nome);
+            id = get_int("", 0, numero_giocatori);
+            if(giocatori[id].p == NULL) {
+                giocatori_veri[scelta].index = id;
+                giocatori[id].p = &giocatori_veri[scelta];
+            } else {
+                printf("\nQuesto id e' gia' preso, inseriscine un altro...");
+                scelto = true;
+            }
+        } while(scelto == true);
+    }
+
+
+
+
+
+
+
+
+    // stampa la lista dei giocatori
 
     printf("\n\n");
     for(i = 0; i < numero_giocatori; i++) {
@@ -169,6 +213,18 @@ int main() {
             printf(" <morto>");
         }
     }
+
+    // stampa i profili giocatore
+
+    printf("\n\n");
+    for(i = 0; i < numero_profili; i++) {
+
+        printf("\n[%d] -> %s", giocatori_veri[i].index, giocatori_veri[i].nome);
+    }
+
+
+
+
 
 
     /*open file template
@@ -205,7 +261,6 @@ int main() {
 
 
     free(giocatori);
-    free(giocatori_vivi);
     free(giocatori_veri);
 
     return 0;
